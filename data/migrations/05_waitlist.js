179335eabad383a1d533c848bdab7d21fea052ec
Brendan <brendanschatz97@gmail.com>
2020-10-07 18:54:49 -0400

    why now no work

--- data/migrations/05_waitlist.js
+++ data/migrations/05_waitlist.js
@@ -8,7 +8,7 @@ exports.up = function (knex) {
             .notNullable();
         table.string("Phone")
         table.timestamp('Time')
-            .defaultTo(NOW())
+            .defaultTo(now())
     });
 };
 
